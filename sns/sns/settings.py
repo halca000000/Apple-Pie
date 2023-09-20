07afff521229fe194ae5fb16252c004969eae2f7
halca000000 <f20144@ichinoseki.kosen-ac.jp>
2023-09-20 11:28:52 +0900

    Django3

--- sns/sns/settings.py
+++ sns/sns/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for sns project.
 
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'sns.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
