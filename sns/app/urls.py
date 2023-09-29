bcd9eff7d8e40f751f2602c7fa0ac1e7043c51b4
tomoyatadano <f20111@g.ichinoseki.ac.jp>
2023-09-29 11:36:54 +0900

    urls.pyにパスを追加しました

--- sns/app/urls.py
+++ sns/app/urls.py
@@ -5,3 +5,9 @@ app_name = "app"
 urlpatterns = [
   path('top/', views.IndexView.as_view(), name='index'),
 ]
+
+urlpatterns = [
+    path('top/', views.IndexView.as_view(), name='index'),
+    path('signup/', views.SignUpView.as_view(), name="signup"),
+    path('home/', views.HomeView.as_view(), name="home"),
+]
