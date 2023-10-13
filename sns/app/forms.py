886b810967aae94aab49424884781191892c9711
halca000000 <f20144@ichinoseki.kosen-ac.jp>
2023-10-13 11:04:56 +0900

    ログイン画面の作成とhtmlファイルの修正

--- sns/app/forms.py
+++ sns/app/forms.py
@@ -1,8 +1,15 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+#ログイン
+from django.contrib.auth.forms import AuthenticationForm 
 
 class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
 
+#
+class LoginForm(AuthenticationForm):  # 追加
+   username = forms.CharField(label='ユーザーネーム') 
+   password = forms.CharField(label='パスワード', widget=forms.PasswordInput) 
--- sns/app/urls.py
+++ sns/app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name="signup"),
     path('home/', views.HomeView.as_view(), name="home"),
     path('profile/', views.ProfileView.as_view(), name='profile'),#追加
+    path('login/', views.UserLoginView.as_view(), name='login'), #ログイン
+    path('logout/', views.UserLogoutView.as_view(), name='logout'), #ログアウト 
 ]
