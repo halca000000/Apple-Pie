4a0c1ec45c44c78a0ef217accc317e68aa4e4d1f
sawahaya <f20085@g.ichinoseki.ac.jp>
2023-10-13 10:00:34 +0900

    urls.pyでpathの追加をしました

--- sns/sns/urls.py
+++ sns/sns/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import test
+from . import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test/', test.test, name='test'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
