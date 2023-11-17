9db09822e4b70be8d33d52ee40d455c95f8cb3d4
sawahaya <f20085@g.ichinoseki.ac.jp>
2023-11-17 11:31:26 +0900

    urls.pyで必要なpathを追加しました。

--- sns/app/urls.py
+++ sns/app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = "app"
 
@@ -12,4 +14,14 @@ urlpatterns = [
     path('logout/', views.UserLogoutView.as_view(), name='logout'), #ログアウト 
     path('edit_profile/', views.ProfileEditView.as_view(), name='edit_profile'), #プロフ編集
     path('userlist/', views.UserListView.as_view(), name='userlist'), #ユーザリスト
+     #ここからさわはやが追加しました
+    path('nav/', views.NavView.as_view(), name="nav"),
+    path('detail/',views.PostDeleteView.as_view(), name="detail"),
+    path('delete/', views.DeleteView.as_view(), name="delete"),
+    path('form/', views.PostCreatedView.as_view(), name="form"),
+    path('delete/',views.PostUpdateView.as_view(), name = "update"),
+    path('commentcr/', views.CommentCreateView.as_view(), name = "commentcr"),
+    path('userpost/',views.UserPostView.as_view(), name = "userpost")
+
+    #ここまでさわはやが追加しました
 ]
